42f55f2322f0be74bc051b73641dcef9743c79a4
namelessbliss <anonimo2911.2012@gmail.com>
2020-02-15 18:16:08 -0500

    Añdiendo models, Item object depende de producto que se obtendra del microservicio productos

---
+++ src/main/java/com/nb/springbootmicroserviceitem/Models/Item.java
@@ -0,0 +1,35 @@
+package com.nb.springbootmicroserviceitem.Models;
+
+public class Item {
+
+    private Producto producto;
+    private int cantidad;
+
+    public Item() {
+    }
+
+    public Item(Producto producto, int cantidad) {
+        this.producto = producto;
+        this.cantidad = cantidad;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getTotal() {
+        return producto.getPrecio() * cantidad;
+    }
+}
