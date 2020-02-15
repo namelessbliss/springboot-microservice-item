c05f5549d10494aca019d4db3f475925d17d542e
namelessbliss <anonimo2911.2012@gmail.com>
2020-02-15 18:10:45 -0500

    Proyecto inciial

---
+++ src/main/java/com/nb/springbootmicroserviceitem/SpringbootMicroserviceItemApplication.java
@@ -0,0 +1,13 @@
+package com.nb.springbootmicroserviceitem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMicroserviceItemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMicroserviceItemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nb/springbootmicroserviceitem/SpringbootMicroserviceItemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nb.springbootmicroserviceitem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMicroserviceItemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
