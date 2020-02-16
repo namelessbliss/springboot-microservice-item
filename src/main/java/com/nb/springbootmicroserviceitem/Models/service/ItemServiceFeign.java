2266adb982f953f69e74555310a03ef980b757e4
namelessbliss <anonimo2911.2012@gmail.com>
2020-02-15 19:40:50 -0500

    Implementando y usando cliente Rest de Feign para consumir api productos

---
+++ src/main/java/com/nb/springbootmicroserviceitem/Models/service/ItemServiceFeign.java
@@ -0,0 +1,28 @@
+package com.nb.springbootmicroserviceitem.Models.service;
+
+import com.nb.springbootmicroserviceitem.Models.Item;
+import com.nb.springbootmicroserviceitem.clientes.ProductoClienteRest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service("serviceFeign")
+@Primary //Permite que se injecte por defecto en el controller
+public class ItemServiceFeign implements ItemService {
+
+    @Autowired
+    private ProductoClienteRest clienteFeign;
+
+    @Override
+    public List<Item> findAll() {
+        return clienteFeign.listar().stream().map(p -> new Item(p, 1)).collect(Collectors.toList());
+    }
+
+    @Override
+    public Item findById(Long id, int cantidad) {
+        return new Item(clienteFeign.detalle(id), cantidad);
+    }
+}
---
+++ src/main/java/com/nb/springbootmicroserviceitem/clientes/ProductoClienteRest.java
@@ -0,0 +1,19 @@
+package com.nb.springbootmicroserviceitem.clientes;
+
+import com.nb.springbootmicroserviceitem.Models.Item;
+import com.nb.springbootmicroserviceitem.Models.Producto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "servicio-productos", url = "localhost:8087")
+public interface ProductoClienteRest {
+
+    @GetMapping("/listar")
+    public List<Producto> listar();
+
+    @GetMapping("/ver/{id}")
+    public Producto detalle(@PathVariable long id);
+}
