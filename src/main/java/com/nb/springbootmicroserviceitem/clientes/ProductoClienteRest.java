589df21907ef69efe58166334c7d991f400aaff9
namelessbliss <anonimo2911.2012@gmail.com>
2020-02-16 21:14:05 -0500

    Implementando balanceo de carga de lado del cliente con Ribbon

--- src/main/java/com/nb/springbootmicroserviceitem/clientes/ProductoClienteRest.java
+++ src/main/java/com/nb/springbootmicroserviceitem/clientes/ProductoClienteRest.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "servicio-productos", url = "localhost:8087")
+@FeignClient(name = "servicio-productos")
 public interface ProductoClienteRest {
 
     @GetMapping("/listar")
