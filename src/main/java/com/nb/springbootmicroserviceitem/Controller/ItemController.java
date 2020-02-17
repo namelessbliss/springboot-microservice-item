84c025ac58c419331e1b3508830e0392e98c1343
namelessbliss <anonimo2911.2012@gmail.com>
2020-02-17 11:19:35 -0500

    Implementando tolerancia a fallos y latencia con Hystrix

--- src/main/java/com/nb/springbootmicroserviceitem/Controller/ItemController.java
+++ src/main/java/com/nb/springbootmicroserviceitem/Controller/ItemController.java
@@ -1,7 +1,10 @@
 package com.nb.springbootmicroserviceitem.Controller;
 
 import com.nb.springbootmicroserviceitem.Models.Item;
+import com.nb.springbootmicroserviceitem.Models.Producto;
 import com.nb.springbootmicroserviceitem.Models.service.ItemService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,8 +25,20 @@ public class ItemController {
         return itemService.findAll();
     }
 
+    @HystrixCommand(fallbackMethod = "metodoAlternativo")
     @GetMapping("/ver/{id}/cantidad/{cantidad}")
     public Item detalle(@PathVariable long id, @PathVariable int cantidad) {
         return itemService.findById(id, cantidad);
+    }
+
+    public Item metodoAlternativo(long id, int cantidad) {
+        Item item = new Item();
+        Producto producto = new Producto();
+        item.setCantidad(cantidad);
+        producto.setId(id);
+        producto.setNombre("Camara");
+        producto.setPrecio(500.0);
+        item.setProducto(producto);
+        return item;
     }
 }
